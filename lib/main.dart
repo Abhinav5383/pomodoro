937ce833c115b3888c687cea3eb1b5a446dad7bd
Abhinav5383 <itsabhinav5383@gmail.com>
2026-03-23 14:39:40 +0530

    make the contents scrollable on low height use LayoutBuilder to set minHeight and center the contents

--- lib/main.dart
+++ lib/main.dart
@@ -26,46 +26,66 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final base = Theme.of(context);
 
-    return Container(
-      decoration: BoxDecoration(color: base.scaffoldBackgroundColor),
-      child: Column(
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+        decoration: BoxDecoration(color: base.scaffoldBackgroundColor),
+
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(48),
+
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   spacing: 32,
 
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: base.cardColor,
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: base.cardColor,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
 
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
 
-              child: Text(
-                "00:00",
-                style: TextStyle(
-                  color: base.colorScheme.primary,
-                  decoration: TextDecoration.none,
-                  fontSize: 48,
+                        child: Text(
+                          "00:00",
+                          style: TextStyle(
+                            color: base.colorScheme.primary,
+                            decoration: TextDecoration.none,
+                            fontSize: 48,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: base.buttonTheme.colorScheme!.primary,
+                        foregroundColor:
+                            base.buttonTheme.colorScheme!.onPrimary,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 16,
+                        ),
+                      ),
+
+                      onPressed: () {
+                        print("btn clicked");
+                      },
+                      child: const Text("Start"),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-
-          TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: base.buttonTheme.colorScheme!.primary,
-              foregroundColor: base.buttonTheme.colorScheme!.onPrimary,
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            ),
-
-            onPressed: () {
-              print("btn clicked");
-            },
-            child: Text("Start"),
-          ),
-        ],
+        ),
       ),
     );
   }
